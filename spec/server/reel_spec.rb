fcf533731397947610da7eddb48e5cd32d987006
mashiro <mail@mashiro.org>
2013-05-09 16:11:08 +0900

    follow saorin-0.5.0

--- spec/server/reel_spec.rb
+++ spec/server/reel_spec.rb
@@ -3,6 +3,6 @@ require 'saorin/test'
 
 describe Saorin::Server::Reel do
   include Saorin::Test
-  include_context 'setup rpc server client', :server => :reel
+  include_context 'setup rpc server client', :server => {:adapter => :reel}
   it_should_behave_like 'rpc communicatable'
 end
